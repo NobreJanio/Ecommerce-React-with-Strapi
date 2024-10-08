a60569b05665f587aaef1e97644885ec25030fe9
NobreJanio <janionobrega@hotmail.com>
2024-10-08 19:31:01 -0300

    f7

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,16 +1,18 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: env('URL', 'http://localhost:1337'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  url: env('URL', 'https://ecommerce-react-with-strapi.onrender.com'), // URL do backend no Render
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
-
   cors: {
     enabled: true,
-    origin: env('CORS_ORIGIN', ['http://localhost:5173']),
+    origin: ['https://ecommerce-90e52.web.app'], // URL do frontend
   },
 });
+
